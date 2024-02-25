97bf9ed6abf0d62e9ec994db33f04afacf6c55cb
Fayee20 <99829537+Fayee20@users.noreply.github.com>
2024-02-26 03:01:13 +0800

    Add files via upload

---
+++ navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { YoutubeService } from '../youtubeservice.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  searchQuery: string = '';
+
+  constructor(private youtubeService: YoutubeService, private router: Router) {}
+
+  searchVideos() {
+    if (this.searchQuery.trim() !== '') {
+      this.router.navigate(['/search'], { queryParams: { q: this.searchQuery } });
+    }
+  }
+}
+
